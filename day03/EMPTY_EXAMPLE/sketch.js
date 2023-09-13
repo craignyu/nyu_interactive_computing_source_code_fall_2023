317d21d1a67368aeb097aa1f715b14d519eb7294
craignyu <kapp@cs.nyu.edu>
2023-09-13 15:21:16 -0400

    day03 - fixed empty example

--- day03/EMPTY_EXAMPLE/sketch.js
+++ day03/EMPTY_EXAMPLE/sketch.js
@@ -1,34 +1,9 @@
-let r = 255;
-let g = 255;
-let b = 255;
-
 // setup function - used for commands that need to run only once
 function setup() {
   createCanvas(500,500);
-  background(0);
-  strokeWeight(10);
 }
 
 // draw function - used for commands that need to be repeated
 function draw() {
-
-  if (mouseIsPressed == true) {
-    stroke(r,g,b);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
-
-}
-
-function keyPressed() {
-
-  // check if the 'C' key was pressed
-  if (key == 'c' || key == 'C') {
-    r = random(0,255);
-    g = random(0,255);
-    b = random(0,255);
-
-    fill(r,g,b);
-    rect(0,0,20,20);
-  }
 
 }
